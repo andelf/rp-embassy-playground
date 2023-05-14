3b24c0a75ec681ef52e137e95bb13a23b7d4d6a2
Andelf <andelf@gmail.com>
2023-05-14 11:51:50 +0800

    add adc-thermistor

---
+++ src/bin/adc-thermistor.rs
@@ -0,0 +1,47 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::adc::{Adc, Config};
+use embassy_rp::interrupt;
+use embassy_time::{Duration, Timer};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let irq = interrupt::take!(ADC_IRQ_FIFO);
+    let mut adc = Adc::new(p.ADC, irq, Config::default());
+
+    let mut p26 = p.PIN_26; // A0
+                            //    let mut p27 = p.PIN_27;
+                            //  let mut p28 = p.PIN_28;
+
+    loop {
+        let level = adc.read(&mut p26).await; // 12 bit ADC, max value is 4095
+        let raw_10bit = level >> 2;
+
+        let temp = rp::thermistor::analog_to_temp(raw_10bit);
+
+        info!("thermistor: {} {}℃", raw_10bit, temp);
+        //    let level = adc.read(&mut p27).await;
+        //  info!("Pin 27 ADC: {}", level);
+        //let level = adc.read(&mut p28).await;
+        //info!("Pin 28 ADC: {}", level);
+        //  let temp = adc.read_temperature().await;
+        // info!("Temp: {} degrees", convert_to_celsius(temp));
+        Timer::after(Duration::from_secs(1)).await;
+    }
+}
+
+fn convert_to_celsius(raw_temp: u16) -> f32 {
+    // According to chapter 4.9.5. Temperature Sensor in RP2040 datasheet
+    27.0 - (raw_temp as f32 * 3.3 / 4096.0 - 0.706) / 0.001721 as f32
+}
+
+fn convert_to_voltage(raw_value: u16) -> f32 {
+    // According to chapter 4.9.5. Temperature Sensor in RP2040 datasheet
+    (raw_value as f32 * 3.3 / 4096.0) as f32
+}
---
+++ src/thermistor.rs
@@ -0,0 +1,57 @@
+/// 100k ParCan thermistor (104GT-2)
+/// ATC Semitec 104GT-2 (Used in ParCan)
+pub const TEMPTABLE_5: [[u16; 2]; 32] = [
+    [1, 713],
+    [17, 300], // top rating 300C
+    [20, 290],
+    [23, 280],
+    [27, 270],
+    [31, 260],
+    [37, 250],
+    [43, 240],
+    [51, 230],
+    [61, 220],
+    [73, 210],
+    [87, 200],
+    [106, 190],
+    [128, 180],
+    [155, 170],
+    [189, 160],
+    [230, 150],
+    [278, 140],
+    [336, 130],
+    [402, 120],
+    [476, 110],
+    [554, 100],
+    [635, 90],
+    [713, 80],
+    [784, 70],
+    [846, 60],
+    [897, 50],
+    [937, 40],
+    [966, 30],
+    [986, 20],
+    [1000, 10],
+    [1010, 0],
+];
+
+pub fn analog_to_temp(raw: u16) -> f32 {
+    // use bisect to find the temperature
+    let mut min = 0;
+    let mut max = TEMPTABLE_5.len() - 1;
+    let mut mid = 0;
+    while max - min > 1 {
+        mid = (max + min) / 2;
+        if TEMPTABLE_5[mid][0] > raw {
+            max = mid;
+        } else {
+            min = mid;
+        }
+    }
+    let min_temp = TEMPTABLE_5[min][1] as f32;
+    let max_temp = TEMPTABLE_5[max][1] as f32;
+    let min_raw = TEMPTABLE_5[min][0] as f32;
+    let max_raw = TEMPTABLE_5[max][0] as f32;
+    let temp = min_temp + (max_temp - min_temp) * (raw as f32 - min_raw) / (max_raw - min_raw);
+    temp
+}
